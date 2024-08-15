8f6db2d48908645b282a492a7087db86e884848b
captivecow <captivecow.dev@gmail.com>
2024-08-14 23:52:55 -0500

    feat: Setup default window with JFrame

--- src/main/java/io/github/captivecow/JMultiChat.java
+++ src/main/java/io/github/captivecow/JMultiChat.java
@@ -1,13 +1,23 @@
 package io.github.captivecow;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+import java.awt.*;
+
 public class JMultiChat
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("jMultiChat");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setPreferredSize(new Dimension(800, 600));
+
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
