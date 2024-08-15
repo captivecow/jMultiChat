e648db8d5ca1d95b55c48f718d966b58fc3c0bec
captivecow <captivecow.dev@gmail.com>
2024-08-14 23:36:36 -0500

    feat: Initial commit

---
+++ src/main/java/io/github/captivecow/JMultiChat.java
@@ -0,0 +1,13 @@
+package io.github.captivecow;
+
+/**
+ * Hello world!
+ *
+ */
+public class JMultiChat
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
